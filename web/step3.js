11898cccd66324f0e25b9c1835dd3693aacdf552
hdewett <hdewett@bossinsights.com>
2023-06-06 22:34:34 -0400

    display invoice list and data

--- web/step3.js
+++ web/step3.js
@@ -1,3 +1,5 @@
+import {getSessionCookie} from './step2.js';
+
 paymentLink.addEventListener("click", showPaymentLink);
 function showPaymentLink(invoiceID) {
     //shows payment url after button is clicked
@@ -17,6 +19,68 @@ function showPaymentLink(invoiceID) {
     //disables payment button
     paymentLink.setAttribute("disabled", true);
     paymentLink.innerHTML = "Payment Link Sent";
-}
+};
 
-// `https://example.com/${invoiceID}`
+
+const url = 'step3.php?invoices';
+var parsedInvoiceData;
+
+fetch(url, {
+  method: 'GET',
+})
+  .then(response => response.json())
+  .then(data => {
+    let invoiceList = '<ul id="invoiceList" class="list-group mb-3">';
+    for (let invoice of data) {
+
+        let invoiceListItems = 
+        `<li class="list-group-item d-flex justify-content-between lh-sm" data-invoice-json="${encodeURIComponent(JSON.stringify(invoice))}">
+        
+            <div class="selectInvoice form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" value="" name="${invoice.number}"
+                        aria-label="Select invoice">
+            </div>
+            <div class="invoiceInfo flex-grow-1">
+                <div class="d-flex flex-row align-items-center">
+                    <h6 class="my-0">${invoice.number}</h6>
+                    <small class="invoiceDays ms-2 badge badge rounded-pill">${invoice.days}</small>
+                </div>
+                <small class="invoiceCompany text-muted">${invoice.company}</small>
+            </div>
+                <span class="invoiceAmount text-muted">${invoice.amount}</span>
+        </li>`
+
+        invoiceList += invoiceListItems 
+        
+    }
+
+    invoiceList += '</ul>'
+    document.getElementById('invoice-form').innerHTML+=invoiceList;
+
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+
+    checkboxes.forEach(function (checkbox) {
+    checkbox.addEventListener('change', function () {
+        if (this.checked) {
+        // console.log('Checkbox ' + this.name + ' is checked');
+        let invoiceJSON = decodeURIComponent(this.closest('li').dataset.invoiceJson);
+        localStorage.setItem('invoiceJSON', invoiceJSON);
+        
+        // parsedInvoiceData = JSON.parse(invoiceJSON);
+        console.log(invoiceJSON);
+        
+        } else {
+        console.log('Checkbox ' + this.id + ' is not checked');
+        }
+    });
+    });
+
+    
+
+  })
+  .catch(error => {
+    // Handle any errors
+    console.error(error);
+  });
+
--- web/step3.php
+++ web/step3.php
@@ -5,7 +5,6 @@
  */
 
 use GuzzleHttp\Client;
-use Twig\Environment;
 
 require __DIR__ . '/../common.php';
 
@@ -25,52 +24,63 @@ $client = new Client(['verify' => !$selfSigned, 'base_uri' => 'https://' . $_SES
 
 $page = 1;
 $invoices = [];
-do {
-  try {
-	$response = $client->request('GET', '/api/invoices?page=' . $page, [
-	  'auth' => ['admin', $_SESSION['password']],
-	  'headers' => [
-		'User-Agent' => 'BossInsightsApiClient/1.0',
-		'Accept' => 'application/json'
-	  ]
-	]);
-  } catch (Exception $e) {
-	// echo $twig->render('error.twig', array_merge($commonData, [
-	//   'errorType' => 'Error',
-	//   'errorName' => 'failed to communicate with the data api',
-	//   'errorDescription' => 'Exception: ' . $e->getMessage()
-	// ]));
-	throw new Exception($e->getMessage(), $e->getCode(), $e);
-  }
+if (isset($_GET['invoices'])) {
+
+    do {
+    try {
+        $response = $client->request('GET', '/api/invoices?page=' . $page, [
+        'auth' => ['admin', $_SESSION['password']],
+        'headers' => [
+            'User-Agent' => 'BossInsightsApiClient/1.0',
+            'Accept' => 'application/json'
+        ]
+        ]);
+    } catch (Exception $e) {
+        // echo $twig->render('error.twig', array_merge($commonData, [
+        //   'errorType' => 'Error',
+        //   'errorName' => 'failed to communicate with the data api',
+        //   'errorDescription' => 'Exception: ' . $e->getMessage()
+        // ]));
+        throw new Exception($e->getMessage(), $e->getCode(), $e);
+    }
 
 
-  if ($response->getStatusCode() === 200) {
-	$result = json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
-	if ($result === null) {
-	//   echo $twig->render('error.twig', array_merge($commonData, [
-	// 	'errorType' => 'Error',
-	// 	'errorName' => 'invalid data api response',
-	// 	'errorDescription' => 'Response: ' . var_export($response->getBody()->getContents())
-	//   ]));
-	  throw new Exception('invalid data api response');
-	}
-	$resultCount = count($result);
-	foreach ($result as $invoice) {
-	  if ($invoice['balance'] > 0) {
-		$invoices[] = ['number' => $invoice['invoiceNumber'], 'company' => $invoice['customerName'], 'amount' => $invoice['balance'] / 100, 'due' => $invoice['paymentDueDate'], 'days' => (int)round(((time() - strtotime($invoice['paymentDueDate'])) / 86400)), 'billing_email' => $invoice['billingEmail']];
-	  }
-	}
-  } else {
-	// echo $twig->render('error.twig', array_merge($commonData, [
-	//   'errorType' => 'Error',
-	//   'errorName' => 'unable to communicate with data api',
-	//   'errorDescription' => 'received status code ' . $response->getStatusCode() . ' when communicating with data api'
-	// ]));
-	throw new Exception('unable to communicate with data api');
-  }
-  $page++;
-} while ($resultCount > 0);
 
-header('Location: step3.html');
+    if ($response->getStatusCode() === 200) {
+        $result = json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
+        if ($result === null) {
+        //   echo $twig->render('error.twig', array_merge($commonData, [
+        // 	'errorType' => 'Error',
+        // 	'errorName' => 'invalid data api response',
+        // 	'errorDescription' => 'Response: ' . var_export($response->getBody()->getContents())
+        //   ]));
+        throw new Exception('invalid data api response');
+        }
+        $resultCount = count($result);
+        foreach ($result as $invoice) {
+        if ($invoice['balance'] > 0) {
+            $invoices[] = ['number' => $invoice['invoiceNumber'], 'company' => $invoice['customerName'], 'amount' => $invoice['balance'] / 100, 'due' => $invoice['paymentDueDate'], 'days' => (int)round(((time() - strtotime($invoice['paymentDueDate'])) / 86400)), 'billing_email' => $invoice['billingEmail']];
+        }
+        }
+    } else {
+        // echo $twig->render('error.twig', array_merge($commonData, [
+        //   'errorType' => 'Error',
+        //   'errorName' => 'unable to communicate with data api',
+        //   'errorDescription' => 'received status code ' . $response->getStatusCode() . ' when communicating with data api'
+        // ]));
+        throw new Exception('unable to communicate with data api');
+    }
+    $page++;
+    } while ($resultCount > 0);
+        header('Content-type: application/json');
+        echo json_encode($invoices);
+} else {
+    header('Location: step3.html');
+}
+
+
+
+
+
 
 // echo $twig->render('step3.twig', array_merge($commonData, ['LEGAL_NAME' => $_SESSION['legal_name'], 'invoices' => $invoices]));
