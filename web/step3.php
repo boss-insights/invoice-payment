2e27cb78008ecf597e96ea43fe5a1542c20cc822
hdewett <hdewett@bossinsights.com>
2023-06-09 18:54:51 -0400

    refactor

--- web/step3.php
+++ web/step3.php
@@ -36,11 +36,6 @@ if (isset($_GET['invoices'])) {
         ]
         ]);
     } catch (Exception $e) {
-        // echo $twig->render('error.twig', array_merge($commonData, [
-        //   'errorType' => 'Error',
-        //   'errorName' => 'failed to communicate with the data api',
-        //   'errorDescription' => 'Exception: ' . $e->getMessage()
-        // ]));
         throw new Exception($e->getMessage(), $e->getCode(), $e);
     }
 
@@ -49,11 +44,6 @@ if (isset($_GET['invoices'])) {
     if ($response->getStatusCode() === 200) {
         $result = json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
         if ($result === null) {
-        //   echo $twig->render('error.twig', array_merge($commonData, [
-        // 	'errorType' => 'Error',
-        // 	'errorName' => 'invalid data api response',
-        // 	'errorDescription' => 'Response: ' . var_export($response->getBody()->getContents())
-        //   ]));
         throw new Exception('invalid data api response');
         }
         $resultCount = count($result);
@@ -63,11 +53,6 @@ if (isset($_GET['invoices'])) {
         }
         }
     } else {
-        // echo $twig->render('error.twig', array_merge($commonData, [
-        //   'errorType' => 'Error',
-        //   'errorName' => 'unable to communicate with data api',
-        //   'errorDescription' => 'received status code ' . $response->getStatusCode() . ' when communicating with data api'
-        // ]));
         throw new Exception('unable to communicate with data api');
     }
     $page++;
@@ -77,10 +62,3 @@ if (isset($_GET['invoices'])) {
 } else {
     header('Location: step3.html');
 }
-
-
-
-
-
-
-// echo $twig->render('step3.twig', array_merge($commonData, ['LEGAL_NAME' => $_SESSION['legal_name'], 'invoices' => $invoices]));
