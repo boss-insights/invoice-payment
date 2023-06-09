b767beef589b0353c45f90ffeae948c1da989993
hdewett <hdewett@bossinsights.com>
2023-06-09 18:58:19 -0400

    change payment and invoice links to buttons

--- web/step4.js
+++ web/step4.js
@@ -3,9 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
 let parsedInvoiceData = JSON.parse(localStorage.getItem('invoiceJSON'));
 let checkedInvoiceData= `
   <h4 class="mb-3"> Hi ${parsedInvoiceData.company}, your Invoice #${parsedInvoiceData.number} is due for the amount of $${parsedInvoiceData.amount}, please pay using the Pay Now button below.</h4>
-  <hr class="my-4">
-  <a href="./step5.html"><button id="payNow" class="w-100 btn btn-warning btn-lg" type="submit">Pay Now</button></a>`
+  <hr class="my-4">`
   console.log(checkedInvoiceData);
   document.getElementById('invoice-data').innerHTML+=checkedInvoiceData;
 
 });
+
+if (localStorage.getItem("invoiceStatus") == "paid") {
+    document.getElementById("payNow").innerHTML="Invoice Paid";
+    document.getElementById("payNow").setAttribute("disabled", true);
+}
