82a409e3c8817589684ae33c98dae752df70c0c4
hdewett <hdewett@bossinsights.com>
2023-06-12 09:34:32 -0400

    update invoice status as paid when paid

--- web/step6.js
+++ web/step6.js
@@ -1,1 +1,10 @@
-localStorage.setItem("invoiceStatus","paid");
+let parsedInvoiceData = JSON.parse(localStorage.getItem('invoiceJSON'));
+
+// Update the invoice's status to paid.
+let parsedInvoiceStatus = JSON.parse(localStorage.getItem("invoices"));
+parsedInvoiceStatus[parsedInvoiceData["number"]] = "paid";
+localStorage.setItem("invoices",JSON.stringify(parsedInvoiceStatus));
+
+let successMessage = document.getElementById("successMessage");
+
+successMessage.innerHTML = `${parsedInvoiceData.company}, You've successfully paid invoice #${parsedInvoiceData.number}.`;
