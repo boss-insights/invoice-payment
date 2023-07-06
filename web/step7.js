d3a0ac9813be5fa2e8d9911cc31203d83f51c23d
hdewett <hdewett@bossinsights.com>
2023-07-06 16:02:44 -0400

    update invoice status amount styling

--- web/step7.js
+++ web/step7.js
@@ -10,17 +10,20 @@ let customer_name = parsedInvoiceData["company"];
 let parsedInvoiceStatus = JSON.parse(localStorage.getItem("invoices"));
 let currentInvoiceStatus = parsedInvoiceStatus[parsedInvoiceData.number];
 if (currentInvoiceStatus == undefined) {
-    currentInvoiceStatus = "pending";
+    currentInvoiceStatus = "Pending";
 }
 
 
 invoice_status_container.innerHTML = `
-    <h4><strong>Company Name: ${parsedInvoiceData.company}</strong></h4>
-    <h4><strong>Invoice: #${parsedInvoiceData.number}</strong></h4>
-    <h4><strong>Amount due: $${parsedInvoiceData.amount.toFixed(2)}</strong></h4>
-    <h4><strong>Due date: ${parsedInvoiceData.due.slice(0,10)}</strong></h4>
-    <h4><strong>Invoice status: ${currentInvoiceStatus}</strong></h4>`;
+    <div class="invoice-info">
+    <h4 class="text-center mb-4">Invoice Details</h4>
+    <p><strong>Company Name:</strong> ${parsedInvoiceData.company}</p>
+    <p><strong>Invoice:</strong> #${parsedInvoiceData.number}</p>
+    <p><strong>Amount Due:</strong> $${parsedInvoiceData.amount.toLocaleString("en-US")}</p>
+    <p><strong>Due Date:</strong> ${parsedInvoiceData.due.slice(0,10)}</p>
+    <p><strong>Invoice Status:</strong> ${currentInvoiceStatus}</p>
+    </div>`;
 
 
-
+    // toFixed(2)
 
